afe6c95af1d97d66da034d416be5fbebf534acaa
Parth Patel <parth6796@yahoo.com>
2021-11-16 10:28:56 +0000

    added start and lose functions

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("keypress", handleStart, { once: true });
+const title = document.querySelector("[data-title]");
+
+const handleStart = () => {};
+
+const handleLost = () => {};
